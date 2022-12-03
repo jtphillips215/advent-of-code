1e3fb9c58a3c23596b7c109a438e292990999265
jtphillips215 <jtphillips215@gmail.com>
2022-12-03 15:34:02 -0600

    converting player and elf choice into values

--- 2022/Day2/part1.js
+++ 2022/Day2/part1.js
@@ -10,6 +10,28 @@ const rock = 1,
 // variable to track score
 let totalScore = 0;
 
+//variables to trach elf and player choice;
+let elfChoice, playerChoice;
+
 for (let i = 0; i < input.length; i++) {
+  // splitting input into round with an elf and player choice
   let round = input[i].split(" ");
+
+  // assigning the elf choice a score based on Rock, Paper, Scissors
+  if (round[0] == "A") {
+    elfChoice = rock;
+  } else if (round[0] == "B") {
+    elfChoice = paper;
+  } else {
+    elfChoice = scissors;
+  }
+
+  // assigning player choice a score based on Rock, Paper, Scissors
+  if (round[1] == "X") {
+    playerChoice = rock;
+  } else if (round[1] == "Y") {
+    playerChoice = paper;
+  } else {
+    playerChoice = scissors;
+  }
 }

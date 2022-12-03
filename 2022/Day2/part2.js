649136b2b4ee1813b562feceb558dabe35dc5f37
jtphillips215 <jtphillips215@gmail.com>
2022-12-03 17:27:50 -0600

    adding input to part 2 and splitting/trimming it

--- 2022/Day2/part2.js
+++ 2022/Day2/part2.js
@@ -1,1 +1,7 @@
 const { input } = require("./input");
+
+for (let i = 0; i < input.length; i++) {
+  // splitting input into round with an elf and player choice
+  let round = input[i].split(" ");
+  round[1] = round[1].trim();
+}

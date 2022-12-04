a0e3d3c745bc428976379f95d37c3a1045d8349c
jtphillips215 <jtphillips215@gmail.com>
2022-12-03 23:06:15 -0600

    initial set up for part 2

--- 2022/Day2/part2.js
+++ 2022/Day2/part2.js
@@ -1,5 +1,13 @@
 const { input } = require("./input");
 
+// outcomes: x lose, y draw, z win
+
+// values for choices and initial value for total score
+const rock = 1,
+  paper = 2,
+  scissors = 3;
+let totalScore = 0;
+
 for (let i = 0; i < input.length; i++) {
   // splitting input into round with an elf and player choice
   let round = input[i].split(" ");

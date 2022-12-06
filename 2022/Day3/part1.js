fc350d69e03782303c374bba1069310837a347bd
jtphillips215 <jtphillips215@gmail.com>
2022-12-05 20:42:35 -0600

    added function for testing for commonalites, split input strings into substrings

--- 2022/Day3/part1.js
+++ 2022/Day3/part1.js
@@ -1,7 +1,29 @@
 const { input } = require("./input");
 
+function substringTest(firstHalf, lastHalf) {
+  // vector for testing upper and lower matches
+  const MAX_CHAR = 52;
+  let vector = Array(MAX_CHAR).fill(0);
+
+  // increment vector index for every character in the first half
+  for (let i = 0; i < firstHalf.length; i++) {
+    vector[firstHalf[i] - "a"] = true;
+  }
+
+  // checking common substring of the last half in the first half
+  for (let i = 0; i < lastHalf.length; i++) {
+    if (vector[lastHalf[i] - "a"]) return true;
+  }
+  return false;
+}
+
 for (let i = 0; i < input.length; i++) {
   input[i].trim();
+
+  // getting middle index and creating substrings to test for commonalities
+  let middleIndex = input[i].length / 2;
+  let firstHalf = input[i].substring(0, middleIndex);
+  let lastHalf = input[i].substring(middleIndex, input[i].length);
 }
 
 console.log();

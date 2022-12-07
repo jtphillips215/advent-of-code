f147cb9e3cf48897619e5a49e3381f1d1c2f2cfc
jtphillips215 <jtphillips215@gmail.com>
2022-12-06 20:42:18 -0600

    now getting unique characters contained in both halves of string

--- 2022/Day3/part1.js
+++ 2022/Day3/part1.js
@@ -4,6 +4,7 @@ function substringTest(firstHalf, lastHalf) {
   // vector for testing upper and lower matches
   const MAX_CHAR = 52;
   let vector = Array(MAX_CHAR).fill(0);
+  let priority = 0;
 
   // increment vector index for every character in the first half
   for (let i = 0; i < firstHalf.length; i++) {
@@ -12,10 +13,16 @@ function substringTest(firstHalf, lastHalf) {
 
   // checking common substring of the last half in the first half
   for (let i = 0; i < lastHalf.length; i++) {
-    if (vector[lastHalf[i] - "a"]) return true;
+    if (vector[lastHalf[i] - "a"]) {
+      return true;
+    }
   }
   return false;
 }
+
+const ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let commonChars = "";
+let totalPriority = 0;
 
 for (let i = 0; i < input.length; i++) {
   input[i].trim();
@@ -24,6 +31,17 @@ for (let i = 0; i < input.length; i++) {
   let middleIndex = input[i].length / 2;
   let firstHalf = input[i].substring(0, middleIndex);
   let lastHalf = input[i].substring(middleIndex, input[i].length);
+
+  if (substringTest(firstHalf, lastHalf)) {
+    for (let i = 0; i < firstHalf.length; i++) {
+      for (let j = 0; j < firstHalf.length; j++) {
+        if (firstHalf[i] == lastHalf[j]) {
+          commonChars += firstHalf[i];
+        }
+      }
+    }
+    commonChars = String.prototype.concat(...new Set(commonChars));
+  }
 }
 
-console.log();
+console.log(commonChars);

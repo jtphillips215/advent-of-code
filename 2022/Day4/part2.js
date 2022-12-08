abd8dec6544b2802c4728321b447109c4d305d76
jtphillips215 <jtphillips215@gmail.com>
2022-12-07 20:07:10 -0600

    trying or as opposed to and to catch any overlap

--- 2022/Day4/part2.js
+++ 2022/Day4/part2.js
@@ -1,1 +1,23 @@
 const { input } = require("./input");
+
+let counter = 0;
+
+for (let i = 0; i < input.length; i++) {
+  let ranges = input[i].trim().split(",");
+  let firstRange = ranges[0].split("-");
+  let secondRange = ranges[1].split("-");
+
+  if (
+    parseInt(firstRange[0]) <= parseInt(secondRange[0]) ||
+    parseInt(firstRange[1]) >= parseInt(secondRange[1])
+  ) {
+    counter++;
+  } else if (
+    parseInt(secondRange[0]) <= parseInt(firstRange[0]) ||
+    parseInt(secondRange[1]) >= parseInt(firstRange[1])
+  ) {
+    counter++;
+  }
+}
+
+console.log(counter);

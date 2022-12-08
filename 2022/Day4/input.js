b7a220e5e59b2440cb1aa820f15494e2f15dda2f
jtphillips215 <jtphillips215@gmail.com>
2022-12-07 19:37:54 -0600

    input split into ranges

--- 2022/Day4/input.js
+++ 2022/Day4/input.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 
 const input = fs
-  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .readFileSync(path.join(__dirname, "test.txt"), "utf8")
   .toString()
   .trim()
   .split("\n");
--- 2022/Day4/part1.js
+++ 2022/Day4/part1.js
@@ -1,1 +1,6 @@
 const { input } = require("./input");
+
+for (let i = 0; i < input.length; i++) {
+  let ranges = input[i].trim().split(",");
+  console.log(ranges);
+}

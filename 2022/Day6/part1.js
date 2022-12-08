da1027127b26bbb67c6547c6af73f2140f586b1d
jtphillips215 <jtphillips215@gmail.com>
2022-12-08 13:40:35 -0600

    set up function for finding unique characters

--- 2022/Day6/part1.js
+++ 2022/Day6/part1.js
@@ -3,4 +3,13 @@ const { input } = require("./input");
 // converting our input into a string
 characters = String(input);
 
-console.log(characters);
+// testing our characters, starting at i = 3 since we can ignore our first characters
+function findPacketStart(characters) {
+  return;
+}
+// testing our characters, starting at i = 3 since we can ignore our first characters
+
+// calling function and printing output
+let output = findPacketStart(characters);
+
+console.log(findPacketStart(output));

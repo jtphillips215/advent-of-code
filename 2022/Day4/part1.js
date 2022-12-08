ecaf0d94ab3722a9ed675ba742e76e8ad89f344f
jtphillips215 <jtphillips215@gmail.com>
2022-12-07 19:45:30 -0600

    added counter and logic to test ranges

--- 2022/Day4/part1.js
+++ 2022/Day4/part1.js
@@ -1,9 +1,22 @@
 const { input } = require("./input");
 
+let counter = 0;
+
 for (let i = 0; i < input.length; i++) {
   let ranges = input[i].trim().split(",");
   let firstRange = ranges[0].split("-");
   let secondRange = ranges[1].split("-");
 
-  console.log(firstRange, secondRange);
+  if (firstRange[0] <= secondRange[0] && firstRange[1] >= secondRange[1]) {
+    counter++;
+  } else if (
+    secondRange[0] <= firstRange[0] &&
+    secondRange[1] >= secondRange[1]
+  ) {
+    counter++;
+  } else {
+    continue;
+  }
 }
+
+console.log(counter);

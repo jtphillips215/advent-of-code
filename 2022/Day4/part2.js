2b95bf5c1067ec221b07dbe835cdb78f03b51016
jtphillips215 <jtphillips215@gmail.com>
2022-12-07 20:13:13 -0600

    changed logic to see if there is any overlap as opposed to using or

--- 2022/Day4/part2.js
+++ 2022/Day4/part2.js
@@ -8,13 +8,13 @@ for (let i = 0; i < input.length; i++) {
   let secondRange = ranges[1].split("-");
 
   if (
-    parseInt(firstRange[0]) <= parseInt(secondRange[0]) ||
-    parseInt(firstRange[1]) >= parseInt(secondRange[1])
+    parseInt(firstRange[0]) <= parseInt(secondRange[0]) &&
+    parseInt(firstRange[1]) >= parseInt(secondRange[0])
   ) {
     counter++;
   } else if (
-    parseInt(secondRange[0]) <= parseInt(firstRange[0]) ||
-    parseInt(secondRange[1]) >= parseInt(firstRange[1])
+    parseInt(secondRange[0]) <= parseInt(firstRange[0]) &&
+    parseInt(secondRange[1]) >= parseInt(firstRange[0])
   ) {
     counter++;
   }

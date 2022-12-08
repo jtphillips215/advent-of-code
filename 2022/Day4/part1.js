3aef26247aca88a95221b708f86240f8bd10d763
jtphillips215 <jtphillips215@gmail.com>
2022-12-07 19:40:41 -0600

    individual ranges split

--- 2022/Day4/part1.js
+++ 2022/Day4/part1.js
@@ -2,5 +2,8 @@ const { input } = require("./input");
 
 for (let i = 0; i < input.length; i++) {
   let ranges = input[i].trim().split(",");
-  console.log(ranges);
+  let firstRange = ranges[0].split("-");
+  let secondRange = ranges[1].split("-");
+
+  console.log(firstRange, secondRange);
 }

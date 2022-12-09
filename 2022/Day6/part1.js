7741a11519e0f5930a48b97aef93a7302929c7c9
jtphillips215 <jtphillips215@gmail.com>
2022-12-08 19:25:49 -0600

    completed part 1

--- 2022/Day6/part1.js
+++ 2022/Day6/part1.js
@@ -5,11 +5,22 @@ characters = String(input);
 
 // testing our characters, starting at i = 3 since we can ignore our first characters
 function findPacketStart(characters) {
-  return;
+  for (let i = 3; i < characters.length - 1; i++) {
+    let packetCode = characters.substring(i - 4, i);
+    let packetUniqueCount = String.prototype.concat(
+      ...new Set(packetCode)
+    ).length;
+
+    if (packetUniqueCount > 3) {
+      return i;
+    } else {
+      continue;
+    }
+  }
 }
 // testing our characters, starting at i = 3 since we can ignore our first characters
 
 // calling function and printing output
 let output = findPacketStart(characters);
 
-console.log(findPacketStart(output));
+console.log(output);

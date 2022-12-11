eed59b1ecba6b22446b52a43b530d4ab1e006948
jtphillips215 <jtphillips215@gmail.com>
2022-12-10 22:26:53 -0600

    adding some set up stuff for day 4

---
+++ 2022/Day5/part1.js
@@ -0,0 +1,20 @@
+/*
+[B]                     [N]     [H]
+[V]         [P] [T]     [V]     [P]
+[W]     [C] [T] [S]     [H]     [N]
+[T]     [J] [Z] [M] [N] [F]     [L]
+[Q]     [W] [N] [J] [T] [Q] [R] [B]
+[N] [B] [Q] [R] [V] [F] [D] [F] [M]
+[H] [W] [S] [J] [P] [W] [L] [P] [S]
+[D] [D] [T] [F] [G] [B] [B] [H] [Z]
+ 1   2   3   4   5   6   7   8   9 
+*/
+
+let firstStack = ["B", "V", "W", "T", "Q", "N", "H", "D"];
+let secondStack = ["B", "W", "D"];
+let thirdStack = ["C", "J", "W", "Q", "S", "t"];
+let fourthStack = [];
+let fifthStack = [];
+let sixthStack = [];
+let seventhStack = [];
+let eighthStack = [];

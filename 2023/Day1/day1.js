4384c302fde5c3e139875c9e467916bb16cf9bcb
jtphillips215 <jtphillips215@gmail.com>
2023-12-18 17:35:57 -0600

    iterating over lines for numbers

--- 2023/Day1/day1.js
+++ 2023/Day1/day1.js
@@ -2,8 +2,27 @@ const { input } = require("./input");
 
 // Part 1
 getTotal = (input) => {
+  // defining sum to return
   let sum = 0;
-  console.log(input);
+
+  // iterating over input to search for numbers line by line
+  for (let i = 0; i < input.length; i++) {
+    let line = input[i];
+    let numList = [];
+
+    // iterating over line to find numbers
+    for (let j = 0; j < line.length; j++) {
+      if (!isNaN(line[j])) {
+        // isNaN returning \r as number
+        if (line[j] == "\r") {
+          continue;
+        } else {
+          numList.push(line[j]);
+        }
+      }
+    }
+    console.log(numList);
+  }
   return sum;
 };
 
